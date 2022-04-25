5471ef0ae823d596011061942ed154d9082929d7
RShbz <remzisahbaz@outlook.com>
2022-04-26 01:34:27 +0300

    create3

--- product-service/src/main/java/com/sahbazremzi/productservice/model/Product.java
+++ product-service/src/main/java/com/sahbazremzi/productservice/model/Product.java
@@ -4,9 +4,15 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigDecimal;
+
 @Data
 @Document(value = "products")
 public class Product {
     @Id
     private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
 }
